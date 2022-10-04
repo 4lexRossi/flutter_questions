9a0df11b2ce9e7dfa3dcb4b595b2c1019d93d8ea
4lexRossi <devalexrossi@gmail.com>
2022-10-03 22:41:33 -0300

    main file changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(new PerguntaApp());
+main() => runApp(QuestionApp());
 
-class PerguntaApp extends StatelessWidget {
+class QuestionAppState extends State<QuestionApp> {
+  var selectQuestion = 0;
+
+  void answer() {
+    print('ok');
+    setState(() {
+      selectQuestion++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final List<String> questions = ['asd asd', 'fgh fgh'];
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Perguntas'),
+        title: Text('Flutter APP'),
       ),
       body: Column(
-        children: [
-          Text(questions[0]),
+        children: <Widget>[
+          Text(questions[selectQuestion]),
+          ElevatedButton(onPressed: answer, child: Text('anwser 1')),
+          ElevatedButton(onPressed: answer, child: Text('anwser 2')),
+          ElevatedButton(onPressed: answer, child: Text('anwser 3')),
         ],
       ),
     ));
+  }
+}
+
+class QuestionApp extends StatefulWidget {
+  @override
+  QuestionAppState createState() {
+    return QuestionAppState();
   }
 }
